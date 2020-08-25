b32bf32b6de10a9679febe1925febe82f5f83940
zhong.huang1 <zhong.huang1@hp.com>
2020-08-25 21:59:56 +1200

    add soap request

---
+++ ApiDemo/Controllers/ValuesController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            string SaveFileFolder = "Data";
+            string[] files = Directory.GetFiles(SaveFileFolder, "*.json", System.IO.SearchOption.AllDirectories);
+            if ( files.Length <= 0 )
+                return NotFound();
+            string jsonString = "";
+            for(int i=0;i<files.Length;i++)
+            {
+                jsonString += "{\"id\":\""+ files[i].Replace(".json","").Replace("Data\\","")+"\",\"name\":\"" +
+                    ReadJsonConfig(files[i], "name") +"\",\"value\":\""+
+                    ReadJsonConfig(files[i], "value") + "\"}";
+                if (i < files.Length - 1)
+                    jsonString += ",";
+            }
+            return "{\"total\":\""+files.Length.ToString()+"\""+",data["+ jsonString + "]}";
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            string filename = "Data//" + id + ".json";
+            if (System.IO.File.Exists(filename))
+                return "{\"id\":\"" + id + "\",\"name\":\"" +
+                    ReadJsonConfig(filename, "name") + "\",\"value\":\"" +
+                    ReadJsonConfig(filename, "value") + "\"}";
+            return NotFound();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        protected string ReadJsonConfig(string file, string strKey)
+        {
+            var config = new ConfigurationBuilder()
+                 .AddJsonFile(file)
+                 .Build();
+            return config[strKey];
+
+        }
+    }
+}
--- ApiDemo/Startup.cs
+++ ApiDemo/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,8 +9,11 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Models;
+using SoapCore;
 
 namespace WebApplication2
 {
@@ -25,6 +29,7 @@ namespace WebApplication2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.TryAddSingleton<ISampleService, SampleService>();
             services.AddControllers();
         }
 
@@ -46,6 +51,8 @@ namespace WebApplication2
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSoapEndpoint<ISampleService>("/SoapDemoService", new BasicHttpBinding(), SoapSerializer.XmlSerializer);
         }
     }
 }
