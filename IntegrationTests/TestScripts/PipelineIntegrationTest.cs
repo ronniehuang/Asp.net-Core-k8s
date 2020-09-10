b976c23b332a2316a323c8f28b59b6e485d97905
zhong.huang1 <zhong.huang1@hp.com>
2020-09-10 21:07:51 +1200

    add integration test

--- IntegrationTests/TestScripts/PipelineIntegrationTest.cs
+++ IntegrationTests/TestScripts/PipelineIntegrationTest.cs
@@ -46,7 +46,7 @@ namespace IntegrationTests.TestScripts
                 //Then
                 .VerifyHttpStatus(HttpStatusCode.NotFound);
         }
-        [Test(Description = "add Customer details with id not in")]
+        [Test(Description = "add Customer details with name not in")]
         [Category("Customers")]
         public void CreateCustomerDetails_Success()
         {
@@ -58,15 +58,150 @@ namespace IntegrationTests.TestScripts
                 //Then
                 .VerifyHttpStatus(HttpStatusCode.OK)
                 .VerifyCustomersJSONContent()
-                .GetMaxID()
+                .GetNewName()
                 //then
-                .PostCustmoerDetail(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values","Name","Value")
+                .PostCustmoerDetail(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
                 .VerifyHttpStatus(HttpStatusCode.Created)
                 //When
                 .GetAllCustomerDetails(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
                 //Then
                 .VerifyHttpStatus(HttpStatusCode.OK)
-                .VerifyContentInclude("");
+                .VerifyContentIncludeNewName();
+        }
+        [Test(Description = "add Customer details with exists name")]
+        [Category("Customers")]
+        public void CreateCustomerDetails_Fail()
+        {
+            new Serivce.BaseSteps()
+                //Given
+                .SetTestDataByKey("AccessNumber:L1")
+                //When
+                .GetAllCustomerDetails(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
+                //Then
+                .VerifyHttpStatus(HttpStatusCode.OK)
+                .VerifyCustomersJSONContent()
+                .GetRandomName()
+                //then
+                .PostCustmoerDetail(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
+                .VerifyHttpStatus(HttpStatusCode.NotFound);
+        }
+        [Test(Description = "update Customer details with not exists name")]
+        [Category("Customers")]
+        public void UpdateCustomerDetails_Success()
+        {
+            new Serivce.BaseSteps()
+                //Given
+                .SetTestDataByKey("AccessNumber:L1")
+                //When
+                .GetAllCustomerDetails(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
+                //Then
+                .VerifyHttpStatus(HttpStatusCode.OK)
+                .VerifyCustomersJSONContent()
+                .GetRandomName()
+                .GetNewName()
+                //then
+                .PutCustmoerDetail(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
+                .VerifyHttpStatus(HttpStatusCode.OK)
+                //When
+                .GetCustomerDetailsByID(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
+                //Then
+                .VerifyHttpStatus(HttpStatusCode.OK)
+                .VerifyContentIncludeNewName(); 
+        }
+        
+        [Test(Description = "update Customer details with exists name")]
+        [Category("Customers")]
+        public void UpdateCustomerDetails_Fail()
+        {
+            new Serivce.BaseSteps()
+                //Given
+                .SetTestDataByKey("AccessNumber:L1")
+                //When
+                .GetAllCustomerDetails(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
+                //Then
+                .VerifyHttpStatus(HttpStatusCode.OK)
+                .VerifyCustomersJSONContent()
+                .GetSecondName()
+                //then
+                .PutCustmoerDetail(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
+                .VerifyHttpStatus(HttpStatusCode.NotFound);
+        }
+        [Test(Description = "Delete Customer details")]
+        [Category("Customers")]
+        public void DeleteCustomerDetails_Success()
+        {
+            new Serivce.BaseSteps()
+                //Given
+                .SetTestDataByKey("AccessNumber:L1")
+                //When
+                .GetAllCustomerDetails(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
+                //Then
+                .VerifyHttpStatus(HttpStatusCode.OK)
+                .VerifyCustomersJSONContent()
+                .GetRandomName()
+                //then
+                .DeleteCustmoerDetail(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
+                .VerifyHttpStatus(HttpStatusCode.OK);
+        }
+        [Test(Description = "Delete Customer details with noexists id")]
+        [Category("Customers")]
+        public void DeleteCustomerDetails_Fail()
+        {
+            new Serivce.BaseSteps()
+                //Given
+                .SetTestDataByKey("AccessNumber:L1")
+                //When
+                .GetAllCustomerDetails(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
+                //Then
+                .VerifyHttpStatus(HttpStatusCode.OK)
+                .VerifyCustomersJSONContent()
+                .GetRandomName()
+                //then
+                .DeleteCustmoerDetail(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
+                .VerifyHttpStatus(HttpStatusCode.OK)
+                //then
+                .DeleteCustmoerDetail(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
+                .VerifyHttpStatus(HttpStatusCode.NotFound);
+        }
+        [Test(Description = "GEt Customer details with exists id")]
+        [Category("Customers")]
+        public void GetOneCustomerDetails_Success()
+        {
+            new Serivce.BaseSteps()
+                //Given
+                .SetTestDataByKey("AccessNumber:L1")
+                //When
+                .GetAllCustomerDetails(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
+                //Then
+                .VerifyHttpStatus(HttpStatusCode.OK)
+                .VerifyCustomersJSONContent()
+                .GetRandomName()
+                //When
+                .GetCustomerDetailsByID(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
+                //Then
+                .VerifyHttpStatus(HttpStatusCode.OK)
+                .VerifyCustomerJSONContent();
+        }
+        [Test(Description = "GEt Customer details with not exists id")]
+        [Category("Customers")]
+        public void GetOneCustomerDetails_Fail()
+        {
+            new Serivce.BaseSteps()
+                //Given
+                .SetTestDataByKey("AccessNumber:L1")
+                //When
+                .GetAllCustomerDetails(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
+                //Then
+                .VerifyHttpStatus(HttpStatusCode.OK)
+                .VerifyCustomersJSONContent()
+                .GetRandomName()
+                //then
+                .DeleteCustmoerDetail(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
+                .VerifyHttpStatus(HttpStatusCode.OK)
+                //When
+                .GetCustomerDetailsByID(Serivce.BaseSteps.MicroserviceBaseUrl + "api/values")
+                //Then
+                .VerifyHttpStatus(HttpStatusCode.NotFound);
         }
     }
 }
